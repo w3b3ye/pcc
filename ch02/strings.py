764c3457655cdf5c3976f3a3c865c89d143961ea
w3b3ye <w3b3ye@gmail.com>
2021-01-15 14:27:46 +0000

    Using rstrip

--- ch02/strings.py
+++ ch02/strings.py
@@ -1,5 +1,4 @@
 #Basic print and string functions
- 
 name1 = "Bob Willice"
 name2 = "ADAM PARORE"
 
@@ -9,9 +8,13 @@ print(name1.upper())
 print(name2.lower())
 
 #Using f to format 
-
 firstName1 = "Bob"
 lastName1 = "Willice"
 
 name = f"Hello, {firstName1} {lastName1}!"
 print(name)
+
+#rstrip will remove space at the end (or right) of variable name3, use lstrip() for left and strip() from both side
+name3 = "Julie "
+print(name3+"Hi")
+print(name3.rstrip()+"Hi") 
