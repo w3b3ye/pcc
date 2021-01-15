f0735cbb3260511487af160723e64ca12e1b2b8c
w3b3ye <w3b3ye@gmail.com>
2021-01-15 14:16:26 +0000

    A simple program to understand strings

---
+++ ch02/strings.py
@@ -0,0 +1,17 @@
+#Basic print and string functions
+ 
+name1 = "Bob Willice"
+name2 = "ADAM PARORE"
+
+print(name2.title())
+print(name1.title())
+print(name1.upper())
+print(name2.lower())
+
+#Using f to format 
+
+firstName1 = "Bob"
+lastName1 = "Willice"
+
+name = f"Hello, {firstName1} {lastName1}!"
+print(name)
