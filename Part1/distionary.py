c9a7d1599de47c33a9e79e80f3ea991acf2d3cf5
w3b3ye <w3b3ye@gmail.com>
2021-01-16 17:54:11 +0000

    Updated dictionary example

--- Part1/distionary.py
+++ Part1/distionary.py
@@ -1,4 +1,4 @@
-#A dictionary example
+#A simple dictionary example
 fav_language = {
     'jen': 'c',
     'sarah': 'python',
@@ -17,3 +17,39 @@ print("\n Printing keys and values togerther.")
 for name, value in fav_language.items():
     print(f"{name.title()} loves {value.title()}.")
 
+#A list within a dictionary
+
+fav_languages = {
+    'jen': ['c', 'c#'],
+    'sarah': ['python'],
+    'edward': ['ruby', 'java', 'c++']
+}
+
+for name, languages in fav_languages.items():
+    print(f"\n{name.title()}'s fav languages are.")
+    for language in languages:
+        print(f"\t{language.title()}") 
+
+#A dictionary within a dictionary
+
+users = {
+    'user1':{
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+    },
+
+    'user2':{
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+    },
+}
+for username, user_info in users.items():
+    print(f"\nUsername: {username.title()}")
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info['location']
+
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
+    
