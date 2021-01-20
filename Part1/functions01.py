24413ec5deb3038d3937a8e01ec1290e5a61c48a
w3b3ye <w3b3ye@gmail.com>
2021-01-20 14:16:54 +0400

    Adding another example functions01

---
+++ Part1/functions01.py
@@ -0,0 +1,21 @@
+def print_model(unprinted_designs, completed_models):
+    """
+    Simulate printing each design, until none is left.
+    Move each design to completed_models after printing.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+
+def show_completed_models(completed_model):
+    """Show all the models that were printed."""
+    print("\nThe following models have been printed.")
+    for completed_model in completed_models:
+        print(completed_model)
+
+unprinted_designs = ['AAA','BBB','CCC']
+completed_models = []
+
+print_model(unprinted_designs, completed_models)
+show_completed_models(completed_models)
