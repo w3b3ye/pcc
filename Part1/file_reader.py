2316755446f7d3cff25eee6d0e018df689f7806e
w3b3ye <w3b3ye@gmail.com>
2021-01-28 14:26:21 +0000

    Using loop to read a file

--- Part1/file_reader.py
+++ Part1/file_reader.py
@@ -1,4 +1,20 @@
-#Reading a file
+#Reading a file using 'with'
 with open('pi_digits.txt') as file_object:
     contents = file_object.read()
 print(contents)
+
+#Reading a file 
+file = open('pi_digits.txt','r')
+contents = file.read()
+print(contents)
+file.close()
+
+#When using "with open" you do not need to worry about closing the file though without it you have to close the file by yourself
+
+#Using for loop to read the file
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip()) #Without using rstrip additional blank lines will be added to the output 
+
