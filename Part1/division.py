ab6bd970cdf38a0ab12d35654f24a2486f6730ac
w3b3ye <w3b3ye@gmail.com>
2021-01-29 09:36:34 +0000

    Execption example

---
+++ Part1/division.py
@@ -0,0 +1,16 @@
+print("Enter two number, I will divide for you.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can not divide by zero!")
+    else:
+        print(answer)
