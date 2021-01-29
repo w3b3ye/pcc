3f7a6eee7411257ff59217f511e2ae91b51a2248
w3b3ye <w3b3ye@gmail.com>
2021-01-29 15:47:32 +0000

    Updated reading writing example

--- Part1/rembr_greet.py
+++ Part1/rembr_greet.py
@@ -1,5 +1,6 @@
 #A simple program to save data in a file and then read it
 import json
+from json.decoder import JSONDecodeError
 
 def get_stored_username():
     """Get stored username if available."""
@@ -7,10 +8,18 @@ def get_stored_username():
     try:
         with open(filename) as f:
             username = json.load(f)
-    except FileNotFoundError:
+    except (FileNotFoundError,JSONDecodeError): #If file is empty, Python will throw JSONDecodeError, need to handle both
         return None
     else:
         return username
+
+def get_new_username():
+    """Prompt for a new username:"""
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+    return username
 
 def greet_user():
     """Greet the user by name."""
@@ -18,11 +27,7 @@ def greet_user():
     if username:
         print(f"Welcome back, {username}!")
     else:
-        username = input("What is your name? ")
-        filename = 'username.json'
-        with open(filename, 'w') as f:
-            json.dump(username, f)
-            print(f"We'll remember you when you come back, {username}!")
-        
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
 
 greet_user()
