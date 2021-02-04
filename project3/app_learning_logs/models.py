446347ef08c360bf08532701d900368e11a1a6ed
w3b3ye <w3b3ye@gmail.com>
2021-02-04 04:18:33 +0000

    Added few more pages.

--- project3/app_learning_logs/models.py
+++ project3/app_learning_logs/models.py
@@ -11,6 +11,25 @@ class Topic(models.Model):
         """ Return a string representation of the model.""" 
         return self.text
 
+class Entry(models.Model):
+   """Something specific learned about the topic."""
+   topic = models.ForeignKey(Topic, on_delete=models.CASCADE) #This line means topic is attached with the ID of another record in DB (Topic) and Entries should be 
+   #deleted when Topic is deleted. This is called cascading.
+   text = models.TextField()
+   date_added = models.DateTimeField(auto_now_add=True)
+
+   class Meta: #This is a nested class, inside Entry class. Here we are telling django to use 'Entries' not 'Entrys' when calling 'Entry' class multiple times.
+      verbose_name_plural = 'entries'
+
+   def __str__(self):
+      """Return a string representation of the model."""
+      if len(self.text) >= 50:
+         return(f"{self.text[:50]}...")
+      else:
+          return(f"{self.text[:]}")
+
+
+
 
 
  
---
+++ project3/app_learning_logs/urls.py
@@ -0,0 +1,12 @@
+"""Defines URL patterns for app_learning_logs"""
+
+from django.urls import path
+from . import views
+
+app_name = 'app_learning_logs'
+urlpatterns = [
+    #Home page
+    path('', views.index, name='index'),
+    #Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+]
