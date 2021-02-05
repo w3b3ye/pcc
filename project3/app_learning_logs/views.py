8009a6d045d559b7cb96bc226f4f65f5a5affd9f
w3b3ye <w3b3ye@gmail.com>
2021-02-05 22:28:55 +0000

    Added entry page.

--- project3/app_learning_logs/views.py
+++ project3/app_learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import EntryForm, TopicForm
 
 # Create your views here.
 
@@ -18,4 +19,40 @@ def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {' topic': topic, 'entries': entries}
-    return render( request, 'app_learning_logs/topic.html', context)
+    return render(request, 'app_learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        #No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        #POST data submitted; process data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('app_learning_logs:topics')
+
+    #Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'app_learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        #No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        #Post data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('app_learning_logs:topic', topic_id=topic_id)
+
+    #Display a blank or invalid form.
+    context = {'topic': topic, 'form': form}
+    return render(request, 'app_learning_logs/new_entry.html', context)
